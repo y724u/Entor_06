09a7c7777f19c9c1b60f6c9617c111423b0728c9
y724u <yusukebebebe@gmail.com>
2020-10-24 22:02:04 +0900

    セクション終了

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-$(function() {
-  
-
+document.addEventListener("DOMContentLoaded", function() { 
+  document.getElementById("hamburger_menu").addEventListener("click", function() {this.classList.toggle("active");
+  document.getElementById("nav").classList.toggle("active");
+  })
 });
